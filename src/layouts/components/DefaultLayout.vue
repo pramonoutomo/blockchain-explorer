1be2d4a1a3994925f3a237a17794c8b41dd6bf65
Pramono Utomo <emailbuatcariduit@gmail.com>
2023-07-28 14:42:29 +0700

    Update DefaultLayout.vue

--- src/layouts/components/DefaultLayout.vue
+++ src/layouts/components/DefaultLayout.vue
@@ -81,11 +81,11 @@ function selected(route: any, nav: NavLink) {
         <div
           v-if="isNavGroup(item)"
           :tabindex="index"
-          class="collapse collapse-close"
+          class="collapse"
           :class="{
             'collapse-arrow': item?.children?.length > 0,
             'collapse-close': index === 0 && !sidebarOpen,
-            'collapse-open': index === 0 && sidebarOpen,
+            
           }"
         >
           <input
