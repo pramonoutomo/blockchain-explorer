e0b0e95bc2436808f300547d6003c1e857cbfbd3
Pramono Utomo <emailbuatcariduit@gmail.com>
2024-01-22 09:16:19 +0700

    update

--- src/layouts/components/DefaultLayout.vue
+++ src/layouts/components/DefaultLayout.vue
@@ -276,6 +276,18 @@ function selected(route: any, nav: NavLink) {
 
         <div class="px-4 text-sm pt-2 text-gray-400 pb-2 uppercase">{{ $t('module.links') }}</div>
         <a
+          href="https://explorer.codeblocklabs.com"
+          target="_blank"
+          class="py-2 px-4 flex items-center cursor-pointer rounded-lg hover:bg-gray-100 dark:hover:bg-[#373f59]"
+        >
+          <Icon icon="mdi:chain" class="text-xl mr-2" />
+          <div
+            class="text-base capitalize flex-1 text-gray-600 dark:text-gray-200"
+          >
+            Non-Cosmos Explorer
+          </div>
+        </a>
+        <a
           href="https://twitter.com/CodeBlockLabs"
           target="_blank"
           class="py-2 px-4 flex items-center cursor-pointer rounded-lg hover:bg-gray-100 dark:hover:bg-[#373f59]"
