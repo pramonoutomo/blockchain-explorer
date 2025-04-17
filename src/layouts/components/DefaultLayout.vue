1530f06bb78adb43826d3d06dd262661164da0e6
Pramono Utomo <emailbuatcariduit@gmail.com>
2025-04-17 23:09:02 +0700

    Update DefaultLayout.vue

--- src/layouts/components/DefaultLayout.vue
+++ src/layouts/components/DefaultLayout.vue
@@ -4,9 +4,6 @@ import { computed, ref } from 'vue';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faCoffee } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
-
-library.add(faCoffee);
-
 app.component('font-awesome-icon', FontAwesomeIcon);
 
 // Components
