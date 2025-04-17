6b68fe196b9cd444399a934baf1448edf6cbdb92
Pramono Utomo <emailbuatcariduit@gmail.com>
2025-04-17 23:06:52 +0700

    Update DefaultLayout.vue

--- src/layouts/components/DefaultLayout.vue
+++ src/layouts/components/DefaultLayout.vue
@@ -1,6 +1,13 @@
 <script lang="ts" setup>
 import { Icon } from '@iconify/vue';
 import { computed, ref } from 'vue';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
+library.add(faCoffee);
+
+app.component('font-awesome-icon', FontAwesomeIcon);
 
 // Components
 import newFooter from '@/layouts/components/NavFooter.vue';
@@ -290,7 +297,7 @@ dayjs()
           </div>
         </a>
         <a
-          href="https://github.com/ping-pub/explorer/discussions"
+          href="https://t.me/CodeBlockLabs"
           target="_blank"
           class="py-2 px-4 flex items-center rounded-lg cursor-pointer hover:bg-gray-100 dark:hover:bg-[#373f59]"
         >
@@ -298,7 +305,19 @@ dayjs()
           <div
             class="text-base capitalize flex-1 text-gray-600 dark:text-gray-200"
           >
-            FAQ
+            Community
+          </div>
+        </a>
+        <a
+          href="https://t.me/CodeBlockLabs_chat"
+          target="_blank"
+          class="py-2 px-4 flex items-center rounded-lg cursor-pointer hover:bg-gray-100 dark:hover:bg-[#373f59]"
+        >
+          <font-awesome-icon :icon="['fab', 'telegram']" class="text-xl mr-2" />
+          <div
+            class="text-base capitalize flex-1 text-gray-600 dark:text-gray-200"
+          >
+            Discussion
           </div>
         </a>
       </div>
