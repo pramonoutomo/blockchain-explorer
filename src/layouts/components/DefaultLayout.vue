6118bc956a0509b2d6a08d5df917885a10a3de51
Pramono Utomo <emailbuatcariduit@gmail.com>
2025-04-17 23:25:31 +0700

    Update DefaultLayout.vue

--- src/layouts/components/DefaultLayout.vue
+++ src/layouts/components/DefaultLayout.vue
@@ -278,7 +278,7 @@ dayjs()
         </a>
         <a
           v-if="showDiscord"
-          href="https://discord.com/invite/CmjYVSr6GW"
+          href="#"
           target="_blank"
           class="py-2 px-4 flex items-center rounded-lg cursor-pointer hover:bg-gray-100 dark:hover:bg-[#373f59]"
         >
@@ -306,7 +306,7 @@ dayjs()
           target="_blank"
           class="py-2 px-4 flex items-center rounded-lg cursor-pointer hover:bg-gray-100 dark:hover:bg-[#373f59]"
         >
-          <font-awesome-icon :icon="['fab', 'telegram']" class="text-xl mr-2" />
+          <Icon icon="mdi:telegram" class="text-xl mr-2" />
           <div
             class="text-base capitalize flex-1 text-gray-600 dark:text-gray-200"
           >
